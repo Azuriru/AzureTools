c6637763ba3289a7ea8f215dce6916f3baa0059d
Azuriru <kegumine@gmail.com>
2024-02-22 17:44:57 +0800

    enable pinning items

--- src/lib/components/MaterialSymbol.svelte
+++ src/lib/components/MaterialSymbol.svelte
@@ -5,9 +5,10 @@
     export let type: 'outlined' | 'rounded' | 'sharp' = 'rounded';
     export let name: MaterialSymbol;
     export let size: number | null = null;
+    export let fill: 0 | 1 | boolean = 0;
 </script>
 
-<span class="material-symbol material-symbols-{type}" style:font-size={size + 'px'}>
+<span class="material-symbol material-symbols-{type}" style:font-size={size + 'px'} style:font-variation-settings="'FILL' {+fill}">
     {name}
 </span>
 
