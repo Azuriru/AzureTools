ae488326c456379541607a1fff28ffc86e1e2629
Azuriru <jeannelee823@gmail.com>
2024-02-22 16:47:00 +0800

    +dashboard redirects +minor sidebar qol

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let href = '';
+    export let layout = '';
+</script>
+
+<a {href} class={layout}>
+    <slot />
+</a>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -3,12 +3,13 @@ export { default as FontAwesome } from './FontAwesome.svelte';
 export { default as MaterialSymbol } from './MaterialSymbol.svelte';
 
 // UI wise
+export { default as Link } from './Link.svelte';
+export { default as Button } from './Button.svelte';
+export { default as Input } from './Input.svelte';
+export { default as Textarea } from './Textarea.svelte';
+export { default as Dropdown } from './Dropdown.svelte';
+export { default as Toggle } from './Toggle.svelte';
 export { default as Collapsible } from './Collapsible.svelte';
 export { default as Divider } from './Divider.svelte';
-export { default as Dropdown } from './Dropdown.svelte';
-export { default as Input } from './Input.svelte';
 export { default as Modal } from './Modal.svelte';
 export { default as Popup } from './Popup.svelte';
-export { default as Toggle } from './Toggle.svelte';
-export { default as Button } from './Button.svelte';
-export { default as Textarea } from './Textarea.svelte';
