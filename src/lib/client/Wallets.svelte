de3716e89086e735ac573d6434cea56c34b347b7
Azuriru <jeannelee823@gmail.com>
2024-03-12 12:56:32 +0800

    fix weird double importwallet call

--- src/lib/client/Wallets.svelte
+++ src/lib/client/Wallets.svelte
@@ -45,119 +45,119 @@
     }
 </script>
 
-{#if $currentUser?.wallets.length}
+{#if $currentUser}
     <Column name="wallets" layout="h-full relative">
-        {@const mini = dashboard ? 'text-2xl' : 'text-3xl w-12 h-12'}
-        {@const border = dashboard ? '' : 'rounded border-2 border-solid border-slate-600'}
+        {#if $currentUser.wallets.length}
+            {@const mini = dashboard ? 'text-2xl' : 'text-3xl w-12 h-12'}
+            {@const border = dashboard ? '' : 'rounded border-2 border-solid border-slate-600'}
 
-        <Row name="controls" justify={3} grow={0} layout="mb-2">
-            <Input
-                size={1}
-                class="[&:not(:focus)]:p-0 w-0 focus:w-full {dashboard
-                    ? '!p-0'
-                    : ''}"
-                labelClass="search rounded {border} {dashboard
-                    ? 'has-[:focus]:bg-slate-700'
-                    : ''} has-[:focus]:flex-grow transition-all has-[:focus]:mr-2 overflow-hidden"
-                background=""
-            >
-                <Row
-                    grow={0}
-                    shrink={0}
-                    justify={1}
-                    layout="flex-shrink-0 {mini} pointer-events-none"
+            <Row name="controls" justify={3} grow={0} layout="mb-2">
+                <Input
+                    size={1}
+                    class="[&:not(:focus)]:p-0 w-0 focus:w-full {dashboard
+                        ? '!p-0'
+                        : ''}"
+                    labelClass="search rounded {border} {dashboard
+                        ? 'has-[:focus]:bg-slate-700'
+                        : ''} has-[:focus]:flex-grow transition-all has-[:focus]:mr-2 overflow-hidden"
+                    background=""
                 >
-                    <MaterialSymbol name="search" />
-                </Row>
-            </Input>
-            <Row grow={0} name="controls-right" layout="h-full">
-                <Row name="view" grow={0} layout="h-full rounded {border}">
-                    <Button
-                        onClick={() => (exportShown = true)}
-                        type={0}
-                        bg=""
-                        layout={mini}
-                    >
-                        <MaterialSymbol name="upload_file" fill={dashboard} />
-                    </Button>
-                    {#if !dashboard}
-                        <Divider vr={1} color="border-slate-600" spacing="" />
-                        <Button
-                            onClick={() => (compact = 1)}
-                            type={0}
-                            layout={mini}
-                        >
-                            <MaterialSymbol name="menu" />
-                        </Button>
-                        <Divider vr={1} color="border-slate-600" spacing="" />
-                        <Button
-                            onClick={() => (compact = 0)}
-                            type={0}
-                            layout={mini}
-                        >
-                            <MaterialSymbol name="grid_view" />
-                        </Button>
-                        <Divider vr={1} color="border-slate-600" spacing="" />
-                        <Button
-                            onClick={() => {}}
-                            type={0}
-                            layout={mini}
-                        >
-                            <MaterialSymbol name="settings" />
-                        </Button>
-                    {/if}
-                </Row>
-            </Row>
-        </Row>
-        <Column name="scroller" layout="overflow-auto rounded bg-slate-700">
-            {@const isCompact = compact
-                ? 'flex-col gap-2 p-2'
-                : 'grid gap-4 p-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'}
-            <Column name="wallet-list" layout={isCompact} shrink={0} grow={0}>
-                {#if compact}
                     <Row
-                        name="wallet-header"
                         grow={0}
                         shrink={0}
-                        layout="py-2 pl-4 pr-2 rounded bg-slate-600"
+                        justify={1}
+                        layout="flex-shrink-0 {mini} pointer-events-none"
                     >
-                        <Row name="address" grow={0} layout="w-2/5">
-                            <span class="flex-1 overflow-hidden text-ellipsis">{$t('wallet.address')}</span>
-                        </Row>
-                        <Divider vr={1} spacing="mx-3" />
-                        <Row name="balance" layout="h-full whitespace-nowrap overflow-hidden">
-                            {#each Object.values($clients).map(client => client.chain) as chain, index (chain)}
-                                {#if index !== 0}
-                                    <Divider vr={1} spacing="mx-3" />
-                                {/if}
-                                <span class="flex-1 text-center truncate">
-                                    {getNetworkCurrency(chain?.name) || $t('wallet.currency-unknown')}
-                                </span>
-                            {/each}
-                        </Row>
-                        <Row name="empty" grow={0} layout="w-6 flex-shrink-0" />
+                        <MaterialSymbol name="search" />
                     </Row>
-                {/if}
-                {#each $wallets as wallet (wallet)}
-                    <Wallet client={wallet} {compact} />
-                {/each}
+                </Input>
+                <Row grow={0} name="controls-right" layout="h-full">
+                    <Row name="view" grow={0} layout="h-full rounded {border}">
+                        <Button
+                            onClick={() => (exportShown = true)}
+                            type={0}
+                            bg=""
+                            layout={mini}
+                        >
+                            <MaterialSymbol name="upload_file" fill={dashboard} />
+                        </Button>
+                        {#if !dashboard}
+                            <Divider vr={1} color="border-slate-600" spacing="" />
+                            <Button
+                                onClick={() => (compact = 1)}
+                                type={0}
+                                layout={mini}
+                            >
+                                <MaterialSymbol name="menu" />
+                            </Button>
+                            <Divider vr={1} color="border-slate-600" spacing="" />
+                            <Button
+                                onClick={() => (compact = 0)}
+                                type={0}
+                                layout={mini}
+                            >
+                                <MaterialSymbol name="grid_view" />
+                            </Button>
+                            <Divider vr={1} color="border-slate-600" spacing="" />
+                            <Button
+                                onClick={() => {}}
+                                type={0}
+                                layout={mini}
+                            >
+                                <MaterialSymbol name="settings" />
+                            </Button>
+                        {/if}
+                    </Row>
+                </Row>
+            </Row>
+            <Column name="scroller" layout="overflow-auto rounded bg-slate-700">
+                {@const isCompact = compact
+                    ? 'flex-col gap-2 p-2'
+                    : 'grid gap-4 p-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'}
+                <Column name="wallet-list" layout={isCompact} shrink={0} grow={0}>
+                    {#if compact}
+                        <Row
+                            name="wallet-header"
+                            grow={0}
+                            shrink={0}
+                            layout="py-2 pl-4 pr-2 rounded bg-slate-600"
+                        >
+                            <Row name="address" grow={0} layout="w-2/5">
+                                <span class="flex-1 overflow-hidden text-ellipsis">{$t('wallet.address')}</span>
+                            </Row>
+                            <Divider vr={1} spacing="mx-3" />
+                            <Row name="balance" layout="h-full whitespace-nowrap overflow-hidden">
+                                {#each Object.values($clients).map(client => client.chain) as chain, index (chain)}
+                                    {#if index !== 0}
+                                        <Divider vr={1} spacing="mx-3" />
+                                    {/if}
+                                    <span class="flex-1 text-center truncate">
+                                        {getNetworkCurrency(chain?.name) || $t('wallet.currency-unknown')}
+                                    </span>
+                                {/each}
+                            </Row>
+                            <Row name="empty" grow={0} layout="w-6 flex-shrink-0" />
+                        </Row>
+                    {/if}
+                    {#each $wallets as wallet (wallet)}
+                        <Wallet client={wallet} {compact} />
+                    {/each}
+                </Column>
             </Column>
-        </Column>
+        {:else}
+            <Row
+                name="empty"
+                shrink={0}
+                align={1}
+                justify={1}
+                layout="h-full bg-slate-700 rounded p-4 text-center relative"
+            >
+                {$t('wallets.empty')}
+            </Row>
+        {/if}
         <ImportWallets />
     </Column>
-{:else}
-    <Row
-        name="wallets"
-        shrink={0}
-        align={1}
-        justify={1}
-        layout="h-full bg-slate-700 rounded p-4 text-center relative"
-    >
-        {$t('wallets.empty')}
-        <ImportWallets />
-    </Row>
 {/if}
-
 
 <Modal bind:shown={exportShown} title={$t('wallets.export-wallets')}>
     <Input
